557b5d521d51c166a49016a2d77e1491f4d042a1
sierraaa77 <sierra.lunardon25@ycdsbk12.ca>
2024-03-05 16:52:41 -0500

    complete

--- quiz.py
+++ quiz.py
@@ -1,6 +1,55 @@
 """
 File: quiz.py
-Author: Name
-Date: YYYY-MM-DD
-Description: A brief explanation of what this program does.
+Author: Sierra Lunardon
+Date: 2024-03-05
+Description: This program asks the user questions, checks their answers, then outputs users final scoore.
 """
+
+#print title
+print("Mutilpe-Choice Quiz Game")
+
+#Beginning Score
+score = 0
+
+#Question 1
+print("1. What is the capital of France?")
+print("(a) Paris")
+print("(b) London")
+print("(c) Rome")
+answer=input()
+if answer == "a" or answer == "A":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect.")
+
+#Question 2
+print("2. Which planet is known as the Red Planet?")
+print("(a) Mars")
+print("(b) Venus")
+print("(c) Juipter")
+answer = input()
+if answer == "a" or answer == "A":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect.")
+
+#Question 3
+print("3. Who painted the Mona Lisa?")
+print("(a) Leoardo da Vinci")
+print ("(b) Pablo Piasso")
+print("(c) Vincent van Gogh")
+answer = input()
+if answer == "a" or answer == "A":
+    print("Correct!")
+    score += 1
+else: 
+    print("Incorrect.")
+
+#calculate percentage
+percentage = (score/3) * 100
+
+#Output Users Score
+print("Quiz Complete!")
+print("You answered" ,score, "out of 3 questions correctly. Your score is"  ,int(percentage), "%.")
